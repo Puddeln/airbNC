92a9c1b83c1aba505e52e226b71e349f9a212c1a
puddeln <puddeln@protonmail.com>
2025-01-18 02:27:35 +0000

    updated hosting connections

--- db/connection.js
+++ db/connection.js
@@ -1,3 +1,17 @@
+const ENV = process.env.NODE_ENV || "development";
+const config = {};
+
+// check that either the development/test PGDATABASE variable or the production DATABASE_URL exists
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error("PGDATABASE or DATABASE_URL not set");
+}
+
+if (ENV === "production") {
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
+}
+module.exports = new Pool(config);
+
 const { Pool } = require("pg");
 
 const path = `${__dirname}/../.env.test`;
