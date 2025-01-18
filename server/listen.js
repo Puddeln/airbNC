5f7bc491f87868fd878570eed3d9764983395650
puddeln <puddeln@protonmail.com>
2025-01-18 02:27:35 +0000

    updated hosting connections

--- server/listen.js
+++ server/listen.js
@@ -1,5 +1,9 @@
 const app = require("./app");
 
+// allows the hosting provider to start up the servers
+const { PORT = 9090 } = process.env;
+app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
+
 app.listen(9090, () => {
   console.log("now listening on port 9090!");
 });
